ea91a81c7558045bbb0b04cb6bc7cf44b1279924
Elaine Froehlich <elaine.froehlich@gmail.com>
2018-08-25 15:50:46 +0000

    getting height between range working

--- mario.py
+++ mario.py
@@ -7,13 +7,9 @@ i = 0
 j = 0
 k = 0
 
-# conditional while loop - while True     #prompt user      if condition:     break
-
-# while True:
-# prompt user
-if (height > 0 or height < 6):
-    height -1;
-    break
+# conditional while loop - while True     #prompt user      if condition:     break       WORKING
+if (height < 1 or height > 6):
+    print("Usage: height between 1 and 5")
 
 
 # for every row in height
@@ -21,16 +17,55 @@ if (height > 0 or height < 6):
 # print hashes remainder of spaces - height
 # print a new line
 
-# for height number of rows
-    while i in range(height):
-        print("^ "),
-        # print | for j = height - 1
-        for j in range(height - 1):
-            print("# ")
-            # print remainder of height - j
-            # for k in range(j - height):
-            #     print("# ")
-            #     print()
+# # for height number of rows
+#     while i in range(height - 1):
+#         print("^ ")
+#         height - 1
+#         # print | for j = height - 1
+#         for j in range(height - 1):
+#             print("# ")
+#                 break
+#             # print remainder of height - j
+#             # for k in range(j - height):
+#             #     print("# ")
+#             #     print()
+
+
+
+
+
+
+
+
+# # while True:
+# # prompt user
+# while(height > 0 or height < 6):
+#     break
+
+
+# # for every row in height
+# # print spaces height - 1
+# # print hashes remainder of spaces - height
+# # print a new line
+
+# #for height number of rows
+#     while i in range(height):
+#         for j in range(height-1):
+#             # print("^ ", end =" ")
+#             print("^ ")
+#             if(i == height -1):
+#                 # break
+
+# # # for height number of rows
+# #     while i in range(height):
+# #         print("^ "),
+# #         # print | for j = height - 1
+# #         for j in range(height - 1):
+# #             print("# ")
+# #             # print remainder of height - j
+# #             # for k in range(j - height):
+# #             #     print("# ")
+# #             #     print()
 
 
 
